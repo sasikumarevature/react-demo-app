8f1880a373124cec6b344521389dff2a20fa4dc2
sasikumarevature <sasikumar.manoharan@revature.com>
2024-12-11 12:05:35 +0530

    Bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <>
     <p>Test App</p>
+    <Button variant="primary">
+      Profile <Badge bg="secondary">9</Badge>
+      <span className="visually-hidden">unread messages</span>
+    </Button>
     </>
   );
 }
