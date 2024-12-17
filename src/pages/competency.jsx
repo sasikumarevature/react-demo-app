d996af7ed7b1b2a210eda55f35d08102ec788c12
Shabbir487 <161827262+Shabbir487@users.noreply.github.com>
2024-12-17 14:15:46 +0530

    fixed competency page UI

--- src/pages/competency.jsx
+++ src/pages/competency.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import "./competencies.css";
@@ -19,7 +19,8 @@ const Competency = () => {
     }
   };
   useEffect(() => {
-    fetchData();
+  fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (loading) return <p>Loading...</p>;
@@ -28,7 +29,7 @@ const Competency = () => {
     <>
       <div className="w-full flex justify-end p-3">
         <Link to="/create-competency">
-          <button className="bg-blue-500 w-44 text-white rounded-2xl py-1 px-2 text-base hover:scale-105 hover:bg-blue-600">
+          <button className="bg-blue-800 w-40 text-white rounded-md text-sm p-2 hover:scale-105 hover:bg-blue-600">
             Create Competency
           </button>
         </Link>
@@ -36,27 +37,17 @@ const Competency = () => {
       <div className="card-container px-3">
         {data?.data.map((competency) => (
             <div key={competency.id} className="card">
-              <div className="card-header">
+              <div className="card-header bg-transparent px-1 border-0 py-1 mb-0">
                 <h2>{competency.name}</h2>
-                <span className="duration-badge">
+                <span className="duration-badge bg-blue-800 px-2 py-[0.1rem]">
                   {competency.noOfWeeks} week(s)
                 </span>
               </div>
-              <div className="card-body">
-                <p>
-                  <strong>Type:</strong> {competency.competencyType?.name} (
-                  {competency.competencyType?.description})
-                </p>
-                <p
-                  style={{
-                    color: competency.isDeleted ? "#f44336" : "#4CAF50",
-                  }}
-                >
-                  <strong>Status:</strong>{" "}
-                  {competency.isDeleted ? "Deleted" : "Active"}
+              <div className="card-body px-1">
+                <p className="border w-fit px-1 rounded-md bg-slate-200 text-xs ">{competency.competencyType?.name}
                 </p>
                 <Link to={`/view/${competency.id}`}>
-                  <button className="btn btn-primary">View</button>
+                  <button className="px-3 border-2 border-blue-800 rounded-md text-xs font-semibold py-1 my-2 mb-0 bg-transparent text-blue-800">View</button>
                 </Link>
               </div>
             </div>
